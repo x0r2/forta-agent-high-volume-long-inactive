44886f8d8a3643935daf9b20b85799cf8adc0001
x0r2 <x0r2>
2021-10-22 14:14:20 +0300

    Change etherscan provider

--- src/agent.js
+++ src/agent.js
@@ -1,81 +1,83 @@
-'use strict'
+'use strict';
 
-const ONE_YEAR_IN_SECONDS = 31622400;
-const ONE_ETH = 1000000000000000000; // 1 ETH
 const HIGH_VOLUME = 100000000000000000000; // 100 ETH
-const INACTIVE_INTERVAL = ONE_YEAR_IN_SECONDS; // 1 year
+const INACTIVE_INTERVAL = 31622400; // 1 year
 
 const BigNumber = require('bignumber.js');
-const Etherscan = require('etherscan');
+const EtherscanProvider = require('./etherscan-provider.js');
 
 const {
     Finding,
     FindingSeverity,
-    FindingType,
-    getFortaConfig
+    FindingType
 } = require('forta-agent');
 
-const etherscanApiKey = getFortaConfig().etherscanApiKey;
-const etherscan = new Etherscan(etherscanApiKey);
+const etherscanProvider = new EtherscanProvider();
 
-function provideHandleTransaction(etherscan) {
-    return async function (txEvent) {
+function provideHandleTransaction(etherscanProvider) {
+    return async (txEvent) => {
         const findings = [];
 
-        const from = txEvent.transaction.from;
         const value = new BigNumber(txEvent.transaction.value);
 
-        // Pass low volume
+        // Skip low volume
         if (value.isLessThan(HIGH_VOLUME)) {
             return findings;
         }
 
-        const timestampLast = await getLastTxTimestamp(from);
+        const from = txEvent.transaction.from;
+        const timestampPrev = await getTimestampPrev(from);
 
-        // Pass empty timestamp
-        if (!timestampLast) {
+        // Skip empty timestamp
+        if (!timestampPrev) {
             return findings;
         }
 
         const timestamp = txEvent.block.timestamp;
-        const inactive_time = timestamp - timestampLast;
+        const inactiveTime = timestamp - timestampPrev;
 
-        // Pass short inactive time
-        if (inactive_time < INACTIVE_INTERVAL) {
+        // Skip short inactive time
+        if (inactiveTime < INACTIVE_INTERVAL) {
             return findings;
         }
 
-        const volume = value.dividedBy(ONE_ETH); // Volume in ETH
-        const years = Math.floor(inactive_time / ONE_YEAR_IN_SECONDS); // Years count
+        const printableVolume = value.dividedBy(1000000000000000000);
+        const printableDays = Math.floor(inactiveTime / 86400);
 
         findings.push(
             Finding.fromObject({
-                name: 'High volume after long inactive',
-                description: `High volume (${volume} ETH) after more (${years}) years inactive`,
-                alertId: 'FORTA-1',
+                name: 'High Volume Long Inactive',
+                description: `High volume (${printableVolume} ETH) after (${printableDays}) days inactive`,
+                alertId: 'HIGH-VOLUME-LONG-INACTIVE-1',
                 severity: FindingSeverity.Critical,
-                type: FindingType.Suspicious
+                type: FindingType.Suspicious,
+                metadata: {
+                    from,
+                    value,
+                    inactiveTime
+                }
             })
         );
 
         return findings;
     };
 
-    async function getLastTxTimestamp(address) {
-        const txs = await etherscan.getTxList({
-            address,
-            'sort': 'desc'
+    async function getTimestampPrev(address) {
+        const transactions = await etherscanProvider.getHistoryDesc(address).catch(() => {
+            return etherscanProvider.getHistoryDesc(address).catch(() => {
+                return [];
+            });
         });
 
-        if (txs.length < 2) {
+        if (transactions.length < 2) {
             return;
         }
 
-        return Number(txs[1].timeStamp);
+        return transactions[1].timestamp;
     }
 }
 
 module.exports = {
     provideHandleTransaction,
-    handleTransaction: provideHandleTransaction(etherscan)
+    handleTransaction: provideHandleTransaction(etherscanProvider)
 };
